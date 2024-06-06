1f37ab23f44e0cfc4a4ae74014e20fbf2bd049a6
jnskkmhr <junjun.k2012@gmail.com>
2024-06-06 18:31:01 +0900

    add mono launch option

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -3,12 +3,12 @@ xhost +
 IMAGE="openvslam-ros2-humble:latest"
 NAME="openvslam-ros2-humble-container"
 
-docker run --name $NAME -it --rm \
--e "ACCEPT_EULA=Y" --privileged \
+docker run --name $NAME -it --rm --privileged \
+-e "ACCEPT_EULA=Y" \
 -e DISPLAY \
 -e "PRIVACY_CONSENT=Y" \
 --net host \
 -v $HOME/.Xauthority:/root/.Xauthority \
 -v /dev/:/dev/ \
--v $HOME/ros2_ws:/home/ros2_ws \
+-v $HOME/ros2_ws:/ros2_ws \
 $IMAGE
